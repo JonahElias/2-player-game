621b44ed70354d65a846ec3d4c7c7e70f882151e
JonahElias <jonahelias26@gmail.com>
2022-06-23 15:24:06 -0700

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,52 @@
+import engine.StdDraw;
+import gameobjects.Player;
+
+public class Game {
+
+    private final int FPS_CAP = 60; // max FPS
+    private int count; // keeps track of gameloop
+
+
+
+    public static void main(String[] args){
+        new Game();
+    }
+
+
+    public Game(){
+        setup();
+        mainGameLoop();
+    }
+
+    public void setup(){
+        // setup StdDraw
+        StdDraw.setCanvasSize(650, 650);
+        StdDraw.setScale(0, 100);
+
+        // initialize game variables
+        count = 0;
+
+    }
+
+
+    public void mainGameLoop(){
+        while (true){
+            long start = System.currentTimeMillis();
+
+            StdDraw.clear();
+
+            long end = System.currentTimeMillis();
+
+            // frame timing checks
+            long cpu_time = end - start;
+            int cpu_time_int = (1000 / FPS_CAP) - (int) cpu_time;
+            if (cpu_time_int < 0) {
+                cpu_time_int = 1000 / FPS_CAP;
+            }
+            StdDraw.show(cpu_time_int);
+            System.out.println("cpu time: " + cpu_time);
+        }
+    }
+
+
+}
---
+++ src/gameobjects/Player.java
@@ -0,0 +1,16 @@
+package gameobjects;
+
+public class Player{
+
+   private double x;
+   private double y;
+   private double halfWidth;
+   private double halfHeight;
+
+   public Player(){
+       x = 5.0;
+   }
+
+
+   public double getX(){return x;}
+}
